b56a24dcc31fc87026696313436202685d0503bb
Roman C <roman@Romans-MacBook-Pro.local>
2023-12-25 21:23:25 +0100

    Add deploy config

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,12 +20,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ux8(tlmftvw&si@9r53za4y5-&%_-%j5*pngb_g7_gkyc0b)=e'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(','),
+    )
+)
 
 
 # Application definition
